daa1a64aae04f6279a7f31e0faeed8dc05c25d53
ParthTyagi15 <95574752+ParthTyagi15@users.noreply.github.com>
2024-02-12 11:05:50 +0530

    cf contest update

---
+++ C++ progs/CollectingNumbersII.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+// TO-DO
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    int a,b;
+    vector<int>arr(n + 1,0);
+    for(int i = 1; i <= n; i++){
+        int x;
+        cin >> x;
+        arr[x] = i;
+    }
+
+    for(int i = 0; i < m; i++){
+        int a,b;
+        cin >> a >> b;
+        
+    }
+    return 0;
+}
